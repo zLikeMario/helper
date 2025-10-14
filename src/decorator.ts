b1a4ac35ae51fff951e4942032a0e64e1aa3cf3d
Dream <mac@MacBook-Pro.local>
2025-10-14 16:13:36 +0800

    decorator support unpromise value

--- src/decorator.ts
+++ src/decorator.ts
@@ -9,18 +9,27 @@ export function Memoize<Args extends any[]>(
   computeKey: ((...args: Args) => PropertyKey) | PropertyKey = (...args: Args) => args[0],
   isCacheVoid = false
 ) {
-  return function <T>(originalMethod: (...args: Args) => Promise<T>) {
-    const cachedData = new Map<PropertyKey, MaybeUndefined<T>>();
+  return function <T>(originalMethod: (...args: Args) => T) {
+    const cachedData = new Map<PropertyKey, T>();
     let cachedTime = Date.now();
     const isExpired = () => duration > 0 && cachedTime < Date.now();
 
-    async function replacementMethod(this: any, ...args: Args) {
+    function replacementMethod(this: any, ...args: Args) {
       const key = typeof computeKey === "function" ? computeKey(...args) : computeKey;
       if (!cachedData.get(key) || isExpired()) {
         cachedTime = Date.now() + duration;
-        const v = await originalMethod.apply(this, args);
-        if (isCacheVoid || !isUndefined(v)) {
+        const v = originalMethod.apply(this, args);
+        if (v instanceof Promise) {
           cachedData.set(key, v);
+          v.then((vr) => {
+            if (!isCacheVoid && isUndefined(vr)) {
+              cachedData.delete(key);
+            }
+          });
+        } else {
+          if (isCacheVoid || !isUndefined(v)) {
+            cachedData.set(key, v);
+          }
         }
       }
       return cachedData.get(key)!;
